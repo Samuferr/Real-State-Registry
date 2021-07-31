462e5b07f45614cca5f3a89f27afe2e9f65a223c
Samuel Alves Lima Ferreira <samuel@Samuels-MacBook-Air.local>
2021-07-31 18:08:38 -0300

    Navigation created

---
+++ src/components/List.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import {Text, View} from 'react-native'
+
+export default class List extends Component{
+    render() {
+        return (
+            <View>
+                <Text>
+                    LISTA DE IMÓVEIS
+                </Text>
+            </View>
+        )
+    }
+}
---
+++ src/components/Register.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import {Text, View} from 'react-native'
+
+export default class Register extends Component{
+    render() {
+        return (
+            <View>
+                <Text>
+                    Tela de Cadastro
+                </Text>
+            </View>
+        )
+    }
+}
